5e2ae4dc86d35f3e5219e68d34311da64d8b4070
ASVinvest <ariel.salazar.valenzuela@gmail.com>
2025-09-10 15:56:00 -0300

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -333,3 +333,15 @@
 			});
 
 })(jQuery);
+
+<!-- JS del tema -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/jquery.scrollex.min.js"></script>
+<script src="assets/js/browser.min.js"></script>
+<script src="assets/js/breakpoints.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+
+<!-- 👇 NUEVO: fallback para que toda la tile sea clickeable -->
+<script src="assets/js/dw-tiles.js"></script>
